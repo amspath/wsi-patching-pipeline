43aa177cd1766b524bc58d7e4da304221369138b
r.d.vanhoorn <r.d.vanhoorn@amsterdamumc.nl>
2025-09-19 12:53:44 +0200

    Finalized demo script

--- examples/numpy_mem_writer_demo.py
+++ examples/numpy_mem_writer_demo.py
@@ -10,15 +10,20 @@ def main():
     slides = ["./data/RBIO-GC072-HE-01.tiff", "./data/RBIO-GC072-HE-02.tiff"]
 
     p = (
-        WSIGrid(slides=slides, tile_size=256, stride=256, level=0, use_gpu=True)
+        WSIGrid(slides=slides, tile_size=256, stride=256, level=0, use_gpu=False)
         .then(TilePlanner())
         .then(ReadWindowChunker(max_window_size=8192))
         .then(RegionReadAndBatch(batch_size=800, num_workers=4))
         .to(NumpyMemoryWriter(layout="NCHW"))
     )
 
-    torch_dataset = p.run(cpu_processes=2, profile=False)
-    print(torch_dataset)
+    np_patch_array, np_coords_array, list_of_wsi_ids = p.run(cpu_processes=2, profile=False)
+    print(
+        (
+            f"NumPy dataset: patches shape={np_patch_array.shape}, "
+            f"coords shape={np_coords_array.shape}, wsi_ids count={len(list_of_wsi_ids)}"
+        )
+    )
 
 
 if __name__ == "__main__":
