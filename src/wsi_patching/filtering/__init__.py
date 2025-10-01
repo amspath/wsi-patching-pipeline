c72b2ccfec4802c7a6bf137b1623202c5182509c
Robin van Hoorn <r.d.vanhoorn@amsterdamumc.nl>
2025-10-01 15:13:09 +0200

    Added otsu filter

--- src/wsi_patching/filtering/__init__.py
+++ src/wsi_patching/filtering/__init__.py
@@ -1,5 +1,6 @@
 from .cellvit_tissue_classifier_filter import CellVitTissueClassifierFilter
 from .dummy_tissue_classifier_filter import DummyTissueClassifierFilter
+from .otsu_filter import OtsuFilter
 from .pen_artifact_filter import PenArtifactFilter
 
-__all__ = ["CellVitTissueClassifierFilter", "DummyTissueClassifierFilter", "PenArtifactFilter"]
+__all__ = ["CellVitTissueClassifierFilter", "DummyTissueClassifierFilter", "PenArtifactFilter", "OtsuFilter"]
--- src/wsi_patching/utils/__init__.py
+++ src/wsi_patching/utils/__init__.py
@@ -1,1 +1,3 @@
-__all__ = []
+from .wsi_thumbnail_visualization import visualize_selected_patches
+
+__all__ = ["visualize_selected_patches"]
