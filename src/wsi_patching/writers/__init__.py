79c7ae19a2c73a393555ac7baee0ea6e001693fc
Robin van Hoorn <r.d.vanhoorn@amsterdamumc.nl>
2026-04-21 21:23:34 +0200

    Fix workflow tests

--- src/wsi_patching/writers/__init__.py
+++ src/wsi_patching/writers/__init__.py
@@ -1,6 +1,10 @@
-from .materialize_writers.webdataset.webdataset_loader import WebDatasetLoader
 from .materialize_writers.webdataset.webdataset_writer import WebDatasetWriter
 from .stream_writers.numpy_stream_writer import NumpyStreamWriter
-from .stream_writers.torch_stream_writer import TorchStreamWriter
+
+try:
+    from .materialize_writers.webdataset.webdataset_loader import WebDatasetLoader
+    from .stream_writers.torch_stream_writer import TorchStreamWriter
+except ImportError:
+    pass
 
 __all__ = ["NumpyStreamWriter", "TorchStreamWriter", "WebDatasetWriter", "WebDatasetLoader"]
