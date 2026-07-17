8312a5e5196564c3f03393e011624c0cf960b11c
matthiashagenauer <74023324+matthiashagenauer@users.noreply.github.com>
2026-07-17 13:57:42 +0200

    Update __init__.py

--- src/wsi_patching/filtering/__init__.py
+++ src/wsi_patching/filtering/__init__.py
@@ -2,6 +2,7 @@ from .low_contrast_background_filter import LowContrastBackgroundFilter
 from .otsu_filter import OtsuFilter
 from .pen_artifact_filter import PenArtifactFilter
 from .remove_edge_tiles import RemoveEdgeTiles
+from .segmentation_mask_filter import SegmentationMaskFilter
 
 try:
     from .cellvit_tissue_classifier_filter import CellVitTissueClassifierFilter
@@ -14,4 +15,5 @@ __all__ = [
     "OtsuFilter",
     "PenArtifactFilter",
     "RemoveEdgeTiles",
+    "SegmentationMaskFilter",
 ]
